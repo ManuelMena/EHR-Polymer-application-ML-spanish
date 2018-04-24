b0225348d34c23b366edf5134391e30d76d2b1a2
ManuelMena <openehrmfm@gmail.com>
2018-04-24 11:03:02 -0500

    polymer-firebase

--- google-snomed-cloud-mysql/jquerysnomed.sql
+++
@@ -1,6 +0,0 @@
-use snomed;
-show tables;
-select * from concept limit 1000;
-select * from conceptpreferredname limit 1000;
-select * from conceptwithnames limit 1000;
-select * from relationshipwithnames limit 1000;
